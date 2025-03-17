eb1c018a947b216ed2ac7e8b6ca675197fc43d35
SumanKunwar1 <sumankunwar755@gmail.com>
2025-03-17 14:25:53 +0545

    message something

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,8 +13,8 @@ const Home = () => {
       <BriefAboutSection />
       <ServicesSection />
       <EventsSection />
-      <TestimonialsSection />
       <StatsSection />
+      <TestimonialsSection />
       {/* <LocationSection /> */}
     </div>
   );
