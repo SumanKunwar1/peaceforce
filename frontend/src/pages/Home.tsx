f934be83ee8961bebf647ad7e7e49b81b7da8519
SumanKunwar1 <sumankunwar755@gmail.com>
2025-03-17 13:32:46 +0545

    message something

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import ServicesSection from "../components/home/ServiceSection";
 import EventsSection from "../components/home/ProgramsSection";
 import TestimonialsSection from "../components/home/TestimonialSection";
 import StatsSection from "../components/home/StatSection";
-import LocationSection from "../components/home/LocationSection";
+//import LocationSection from "../components/home/LocationSection";
 
 const Home = () => {
   return (
@@ -15,7 +15,7 @@ const Home = () => {
       <EventsSection />
       <TestimonialsSection />
       <StatsSection />
-      <LocationSection />
+      {/* <LocationSection /> */}
     </div>
   );
 };
