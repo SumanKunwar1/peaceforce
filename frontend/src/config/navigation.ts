12fe8b07f382978c19d8fd03c08ce5af099fb449
Dinesh <ranad4508@gmail.com>
2025-03-17 12:31:05 +0545

    Updated navbar

--- frontend/src/config/navigation.ts
+++ frontend/src/config/navigation.ts
@@ -1,9 +1,9 @@
 export const NavItems = [
   { path: "/", label: "Home" },
   { path: "/about", label: "About" },
-  { path: "/teachings", label: "Prevention Programs" },
+  // { path: "/teachings", label: "Prevention Programs" },
   { path: "/tours", label: "Community Action" },
-  { path: "/events", label: "Events" },
+  // { path: "/events", label: "Events" },
   { path: "/programs", label: "Programs" },
   { path: "/membership", label: "Membership" },
   { path: "/contact", label: "Contact" },
