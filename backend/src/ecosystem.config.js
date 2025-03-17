50a985736a6722afa02fb47ec31a9e18ee8abbf4
Dinesh <ranad4508@gmail.com>
2025-03-17 15:31:46 +0545

    added ecosystem file

--- backend/src/ecosystem.config.js
+++ backend/src/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "peaceforce", // Application name
-      script: "./src/server.ts", // Entry file (make sure it's correct)
+      script: "./server.ts", // Entry file (make sure it's correct)
       interpreter: "ts-node", // Use ts-node as the interpreter
       watch: true, // Optional: watch for file changes
       autorestart: true, // Automatically restart the app if it crashes
